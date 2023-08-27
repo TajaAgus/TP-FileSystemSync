2338f061eeea8d1b5c96f6a6d768d75c6bcbe663
Agus <agustin34tajani@gmail.com>
2023-08-27 16:14:56 -0300

    4 de 6 metodos hechos

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+
+function leerArchivoComoString(ruta){
+    const texto = fs.readFileSync(ruta, 'utf-8')
+    return texto
+}
+
+function escribirTextoEnArchivo(ruta, texto, flag){
+    try
+    {
+        fs.accessSync(ruta) // Chequea que exista el archivo, si no existe lanza error.
+
+        fs.writeFileSync(ruta, texto)
+    }
+    catch (error)
+    {
+        if (flag) {
+            fs.writeFileSync(ruta, texto)
+        } else {
+            throw new Error("El archivo no existe")
+        }
+    }
+}
+
+function transformarStringEnArrayDeNumeros(texto, separador){
+    let textoCortado = texto.split(separador)
+    let numeros = []
+
+    //V1
+    /*
+    for (const texto of textoCortado) {
+        if (!isNaN(parseInt(texto))) numeros.push(texto)
+    }
+    */
+
+    //V2
+    textoCortado.forEach((texto) => /^[0-9]+$/.test(texto) ? numeros.push(texto) : null)
+
+    return numeros 
+}
+
+function transformarArrayDeNumerosAUnSoloString(array, separador){
+    //V1
+    /*
+    let finalString = ""
+    for (let item of array) {
+        finalString += item.toString() + separador
+    }
+    */
+
+    //V2
+    const resultString = array.join(separador)
+    return resultString
+}
+console.log(transformarArrayDeNumerosAUnSoloString([1,2,3,64575,1],'----------'))
---
+++ tests.js
@@ -0,0 +1,1 @@
+console.log(transformarStringEnArrayDeNumeros("H1la-21-Va-123-8-a-.-53231123123", "-"))
