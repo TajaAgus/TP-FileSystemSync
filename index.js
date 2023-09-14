3a9e01fa3e9e257403cdbac34a232189df22944f
Agus <agustin34tajani@gmail.com>
2023-09-13 23:40:30 -0300

    todos los metodos con sus pruebas

--- index.js
+++ index.js
@@ -53,9 +53,42 @@ function transformarArrayDeNumerosAUnSoloString(array, separador){
 }
 
 function combinarDosArrays(arr1, arr2){
-    return arr1.concat(arr2.filter((item) => arr1.indexOf(item) < 0)).sort()
+    return arr1.concat(arr2.filter((item) => arr1.indexOf(item) < 0)).sort((a,b) => a - b)
 }
 
 function combinarNArrays(arr){
     return arr.flat()
 }
+
+
+//leerArchivoComoString
+const ruta = './texto.txt'
+const contenido = leerArchivoComoString(ruta);
+console.log('\n 1- Contenido del archivo:', contenido);
+
+//escribirTextoEnArchivo
+const textoAEscribir = 'Hola Node.js!!!';
+escribirTextoEnArchivo(ruta, textoAEscribir, true);
+console.log('\n 2- Texto escrito en el archivo.');
+
+//transformarStringEnArrayDeNumeros
+const textoAConvertir = '1,2,3,4,5';
+const separador = ',';
+const arrayDeNumeros = transformarStringEnArrayDeNumeros(textoAConvertir, separador);
+console.log('\n 3- Array de números:', arrayDeNumeros);
+
+//transformarArrayDeNumerosAUnSoloString
+const numeros = [1, 2, 3, 4, 5];
+const textoConvertido = transformarArrayDeNumerosAUnSoloString(numeros, separador);
+console.log('\n 4- Texto convertido:', textoConvertido);
+
+//combinarDosArrays
+const arr1 = [1, 2, 3];
+const arr2 = [3, 4, 5];
+const resultadoCombinado = combinarDosArrays(arr1, arr2);
+console.log('\n 5- Resultado combinado de dos arrays:', resultadoCombinado);
+
+//combinarNArrays
+const arr3 = [5, 6];
+const resultadoCombinadoNArrays = combinarNArrays([arr1, arr2, arr3]);
+console.log('\n 6- Resultado combinado de varios arrays:', resultadoCombinadoNArrays);
--- tests.js
+++
@@ -1,2 +0,0 @@
-console.log(transformarStringEnArrayDeNumeros("H1la-21-Va-123-8-a-.-53231123123", "-"))
-console.log(transformarArrayDeNumerosAUnSoloString([1,2,3,64575,1],'----------'))
